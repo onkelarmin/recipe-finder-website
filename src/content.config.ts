620e7eacfc68b8135a58bdf0de2afe5ebe47a0fe
onkelarmin <97581983+onkelarmin@users.noreply.github.com>
2026-01-15 21:33:52 +1000

    built RadioButton Component

---
+++ src/content.config.ts
@@ -0,0 +1,23 @@
+import { file } from "astro/loaders";
+import { defineCollection, z } from "astro:content";
+
+const recipes = defineCollection({
+  loader: file("src/data/data.json"),
+  schema: z.object({
+    id: z.number(),
+    title: z.string(),
+    slug: z.string(),
+    image: z.object({
+      large: z.string(),
+      small: z.string(),
+    }),
+    overview: z.string(),
+    servings: z.number(),
+    prepMinutes: z.number(),
+    cookMinutes: z.number(),
+    ingredients: z.array(z.string()),
+    instructions: z.array(z.string()),
+  }),
+});
+
+export const collections = { recipes };
